42c4a028f1b71a053c6df5430982713e91d11e32
Pardeep-Kumar-Singh <116801261+Pardeep-Kumar-Singh@users.noreply.github.com>
2022-12-05 12:09:38 +0530

    Update style.js

--- module4/style.js
+++ module4/style.js
@@ -1,10 +1,7 @@
 (function () {
-
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
-    
       if (firstLetter === 'j') {
         byeSpeaker(names[i]);
       }
@@ -12,5 +9,4 @@
         helloSpeaker(names[i]);
       }
     }
-    
  })();
